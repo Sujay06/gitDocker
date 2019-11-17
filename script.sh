2ade1fa7b0701c0a5bca2d56aeabd8c86a1694f8
Ubuntu <ubuntu@ip-172-31-18-246.ap-south-1.compute.internal>
2019-11-17 14:22:06 +0000

    requires files for DevOps side server: First Commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+cd /home/ubuntu
+rm -rf dockerImageNode
+git clone https://github.com/sujay06/dockerImageNode.git
+cd dockerImageNode
+sudo docker build -t sujay06/node-app .
+sudo docker run -p 49160:8080 -d sujay06/node-app
